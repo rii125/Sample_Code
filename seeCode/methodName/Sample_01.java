e65b427169ee4e6af88c3b373e26380ccbee50b2
rii_125 <rii125.idd@gmail.com>
2025-04-01 13:37:47 +0900

    Update Sample_01.java

--- seeCode/methodName/Sample_01.java
+++ seeCode/methodName/Sample_01.java
@@ -1,9 +1,14 @@
 public class Example {
   public static void main(String[] args) {
-    int forCount = 10;
-    for (int count; count < forCount; count++) {
-      String serif = "現在" + count + "週目";
-      System.out.println(serif);
+    // forMethodの呼び出し
+    forMethod();
+    
+    public static void forMethod() {
+      int forCount = 10;
+      for (int count; count < forCount; count++) {
+        String serif = "現在" + count + "週目";
+        System.out.println(serif);
+    }
     }
   }
 }
