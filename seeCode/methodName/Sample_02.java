d0fbc61c8af860b6fedbca04ee279eca450cc3ff
rii_125 <rii125.idd@gmail.com>
2025-04-01 13:40:37 +0900

    Update Sample_02.java

--- seeCode/methodName/Sample_02.java
+++ seeCode/methodName/Sample_02.java
@@ -1,1 +1,10 @@
-public class
+public class Example {
+  public static void main(String[] args) {
+    // aaaはどんな処理なのかわからない
+    aaa();
+
+    public static void aaa() {
+      System.out.println("Hello World!");
+    }
+  }
+}
