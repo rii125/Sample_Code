d4f6a8303642535cefe9ceef31b469c9e967e3e8
rii_125 <rii125.idd@gmail.com>
2025-04-01 13:35:21 +0900

    Create Sample_01.java

---
+++ seeCode/methodName/Sample_01.java
@@ -0,0 +1,9 @@
+public class Example {
+  public static void main(String[] args) {
+    int forCount = 10;
+    for (int count; count < forCount; count++) {
+      String serif = "現在" + count + "週目";
+      System.out.println(serif);
+    }
+  }
+}
